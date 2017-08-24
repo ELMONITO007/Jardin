881d07e2826fef78e12514db07da5ec77fe35d3d
BENITEZ <andres.benitez@transener.com.ar>
2017-08-24 18:29:28 -0300

    Listo

--- Jardin De infantes/EE/Servicios/Digito Verificador/DigitoVerificadoV.vb
+++ Jardin De infantes/EE/Servicios/Digito Verificador/DigitoVerificadoV.vb
@@ -1,12 +1,15 @@
 ﻿Public Class DigitoVerificadoV
     Private Property digitos As List(Of String)
-    Private Property nombeTabla As String
+    Private Property nombreTabla As String
+    Private Property digitoHasheado As String
 
-    Sub New(listaDigitos As List(Of String))
+    Sub New(listaDigitos As List(Of String), tabla As String)
         digitos = listaDigitos
-
+        nombreTabla = tabla
     End Sub
 
+    Sub New()
 
+    End Sub
 
 End Class
